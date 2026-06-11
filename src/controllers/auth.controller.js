89f729e2586c07e44fbddadcfb932210cf2c95c3
Jean Paul Ferreira <jeanpaul@jepafe.net>
2026-06-11 16:23:53 -0300

    Login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
@@ -49,5 +50,61 @@ export const register = async (req, res) => {
     });
   } catch (error) {
     res.status(500).json({ message: "Error al registrar al usuario" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const password = String(req.body.password);
+
+    if (!email || !password) {
+      return res
+        .status(422)
+        .json({ message: "Todos los campos son obligatorios" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(422).json({ message: "El correo no es valido" });
+    }
+
+    if (password.length < 5) {
+      return res
+        .status(422)
+        .json({ message: "Contraseña muy corta, mínimo 6 caracteres" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Credenciales invalidas" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Credenciales invalidas" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.json({
+      message: "Login correcto",
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        admin: user.admin,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ mesage: "Error al iniciar sesión" });
   }
 };
